64e543867a8ce736557aad936102de0c950ddc94
Navaneeth K Rameshwaram <nrameshwaram@zscaler.com>
2025-01-10 10:44:06 -0800

    edit todo

--- src/components/BasicTodo.jsx
+++ src/components/BasicTodo.jsx
@@ -4,6 +4,8 @@ import React, { useState } from "react";
 const BasicTodo = () => {
     let [tasks,setTasks] = useState([]);
     let [inputValue, setInputValue] = useState("");
+    let [editText, setEditText] = useState("");
+
 
     const inputHandler = (e) => {
         setInputValue(e.target.value);
@@ -24,6 +26,10 @@ const BasicTodo = () => {
         setInputValue("");
     }
 
+    const editTaskAction = (index) => {
+        editText
+    }
+
 
     return (
         <div>
@@ -32,16 +38,16 @@ const BasicTodo = () => {
             <form onSubmit={ actionHandler }>
                 <label>Task Input *</label><br></br><br></br>
                 <input type="text" placeholder="Enter Task ..." value={inputValue} onChange={inputHandler}/>
-                
                 <button className="btn variant-cta" type="submit">Add</button>
-                
             </form>
+            <input type="text" placeholder="Edit Text ..." value={editText} onChange={(e) => setEditText(e.target.value)}/>
         </div>
         <div>
             <ul aria-label="Task List" height="size-3000">
                 {tasks.map((task, index) => (
                    <li key= { index }>
                         { task }
+                        <button onClick={() => editTaskAction(index)}>Edit</button>
                         <button onClick={() => deleteTaskAction(index)}>Delete</button>
                    </li>
                 ))}
