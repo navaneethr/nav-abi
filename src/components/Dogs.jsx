eed6263aa552c86eb5d3688e9ed7c238656cd245
Navaneeth K Rameshwaram <nrameshwaram@zscaler.com>
2025-02-04 21:10:03 -0800

    dogs page

--- src/components/Dogs.jsx
+++ src/components/Dogs.jsx
@@ -1,12 +1,63 @@
-// src/components/Home.jsx
-import React from "react";
+import React, { useState } from "react";
+import dogs from "../data/dogs.json"
 
 const Dogs = () => {
+    const [showDrawer, setShowDrawer] = useState(false)
+    const [selectedDog, setSelectedDog] = useState(null);
+
+    const onViewMore = (data) => {
+        setShowDrawer(true)
+        // What is this line ?
+        setSelectedDog(data);
+    }
+
+    const onBackClick = () => {
+        setShowDrawer(false)
+        // Why are we setting this to be Null
+        setSelectedDog(null);
+    }
+
   return (
-    <div>
+    <div style={{display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center"}}>
       <h1>Dogs</h1>
+      <div style={{display: "flex", flexWrap: "wrap", justifyContent: "center"}}>
+        {
+            dogs.map((dog, index) => {
+                return (
+                    <Dog key={index} imgUrl={dog.image_url} breed={dog.breed} onViewMore={() => onViewMore(dog)}/>
+                )
+            })
+        }
+      </div>
+      {/* What is && operator */}
+      {
+        showDrawer && <SideDrawer onBackClick={onBackClick} selectedDog={selectedDog}/>
+      }
     </div>
   );
 };
+
+const Dog = ({imgUrl, breed, onViewMore}) => {
+    return (
+        <div style={{display: "flex", justifyContent: "center", alignItems: "center", width: "300px", height: "300px", border: "1px solid", margin: "5px"}}>
+            Why is this imgUrl instead of image_url
+            <img src={imgUrl} height="100"/>
+            <div>{breed}</div>
+            <button onClick={onViewMore}>View More</button>
+        </div>
+    )
+}
+
+const SideDrawer = ({onBackClick, selectedDog}) => {
+    // What is selectedDog above
+    return (
+        <div style={{ height: "100vh", width: "60%", background: "#cacaca", position: "fixed", top: 0, bottom: 0, right: 0, padding: "10px"}}>
+            <div>
+                <button onClick={onBackClick}>Back</button>
+            </div>
+            Name: {selectedDog.breed}
+        </div>
+    )
+}
 
 export default Dogs;
